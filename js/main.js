c17dcad4ac8f0794b9df8309e991e3ef69e0ef86
cadenza13 <99175855+cadenza13@users.noreply.github.com>
2022-02-17 00:41:53 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,62 @@
 'use strict';
 
 {
+  const logo = document.querySelector('.logo');
+  const newBody = document.querySelector('.new-body');
+  const mask = document.querySelector('.mask');
+  let observeStart = false;
+
+  function opacity(){
+    logo.classList.remove('opa');
+  }
+
+  function outFlame(){
+    logo.classList.remove('center');
+  }
+
+  function showTime(){
+    newBody.classList.remove('hidden');
+    mask.classList.add('opa');
+    logo.classList.add('border');
+    observeStart = true;
+  }
+
+  setTimeout(opacity, 100);
+  setTimeout(outFlame, 2000);
+  setTimeout(showTime, 3000);
+
+  const span = document.querySelector('span');
+  const spider = document.getElementById('spider');
+  const target = document.querySelector('header');
+  let encounter = false;
+
+  function callback(entries){
+    if(observeStart === false){
+      return;
+    }
+    if(!entries[0].isIntersecting){
+      if(encounter === true){
+        return;
+      }
+      encounter = true;
+      span.classList.add('down');
+    }
+  }
+
+  function spiderUppear(){
+    span.classList.remove('down');
+  }
+
+  span.addEventListener('click', () =>{
+    spider.src = 'img/spider2.PNG';
+    setTimeout(spiderUppear, 1000);
+  });
+
+  const observer = new IntersectionObserver(callback);
+  observer.observe(target);
+}
+
+{
   const box = document.querySelector('ul');
   let slideIndex = 0;
   
@@ -25,33 +81,4 @@
       topic[index].classList.toggle('hidden');
     });
   });
-}
-
-{
-  const span = document.querySelector('span');
-  const spider = document.getElementById('spider');
-  const target = document.querySelector('header');
-  let encounter = false;
-
-  function callback(entries){
-    if(!entries[0].isIntersecting){
-      if(encounter === true){
-        return;
-      }
-      encounter = true;
-      span.classList.add('down');
-    }
-  }
-
-  function spiderUppear(){
-    span.classList.remove('down');
-  }
-
-  span.addEventListener('click', () =>{
-    spider.src = 'img/spider2.PNG';
-    setTimeout(spiderUppear, 1000);
-  });
-
-  const observer = new IntersectionObserver(callback);
-  observer.observe(target);
 }
